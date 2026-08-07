815fe917239bdb258ad1536b644c8fd3f3aa8e85
thipaiares <thiago.paiares@gmail.com>
2026-08-07 17:41:49 -0300

    feat: update fp_adder_test and hex_to_sseg for improved functionality and display output

--- src/fp_adder_test.vhd
+++ src/fp_adder_test.vhd
@@ -6,11 +6,14 @@ use work.all;
 
 entity fp_adder_test is
     port (
-        clk : in std_logic;
-        sw : in std_logic_vector(7 downto 0);
-        btn : in std_logic_vector(3 downto 0);
-        an : out std_logic_vector(3 downto 0);
-        sseg : out std_logic_vector(7 downto 0)
+        sw : in  std_logic_vector(9 downto 0);  -- os switches
+        key : in  std_logic_vector(1 downto 0);  -- as chaves
+        hex0 : out std_logic_vector(7 downto 0);  -- expoente do resultado
+        hex1 : out std_logic_vector(7 downto 0);  -- letra E fixa
+        hex2 : out std_logic_vector(7 downto 0);  -- fracao, 4 LSBs
+        hex3 : out std_logic_vector(7 downto 0);  -- fracao, 4 MSBs
+		  hex4 : out std_logic_vector(7 downto 0);  -- digito zero
+        hex5 : out std_logic_vector(7 downto 0)   -- sinal do resultado
     );
 end fp_adder_test;
 
@@ -21,15 +24,15 @@ architecture arch of fp_adder_test is
     signal sign_out : std_logic;
     signal exp_out : std_logic_vector(3 downto 0);
     signal frac_out : std_logic_vector(7 downto 0);
-    signal led3, led2, led1, led0 : std_logic_vector(7 downto 0);
 begin
     -- set up the fp adder input signals
     sign1 <= '0';
-    exp1 <= "1000";
-    frac1 <= '1' & sw(1) & sw(0) & "10101";
-    sign2 <= sw(7);
-    exp2 <= btn;
-    frac2 <= '1' & sw(6 downto 0);
+    exp1  <= '1' & (not key(1)) & (not key(0)) & '1'; --"1000";
+    frac1 <= '1' & sw(9 downto 7) & "1101";            --'1' & sw(1) & sw(0) & "10101";
+
+    sign2 <= sw(5);                                    --sw(7);
+    exp2  <= '1' & sw(6) & "00";                         --sw(9) & sw(8) & (not key(1)) & (not key(0));
+    frac2 <= '1' & sw(4 downto 0) & "10";              --'1' & sw(6 downto 0);
 
     -- instantiate fp adder
     fp_add_unit : entity work.fp_adder
@@ -43,25 +46,21 @@ begin
     -- instantiate three instances of hex decoders
     -- exponent
     sseg_unit_0 : entity work.hex_to_sseg
-        port map (hex => exp_out, dp => '0', sseg => led0);
+        port map (hex => exp_out, dp => '1', sseg => hex0);
 
+	 hex1 <= "10000110"; --caractere E
+		  
     -- 4 LSBs of fraction
     sseg_unit_1 : entity work.hex_to_sseg
-        port map (hex => frac_out(3 downto 0), dp => '1', sseg => led1);
+        port map (hex => frac_out(3 downto 0), dp => '1', sseg => hex2);
 
     -- 4 MSBs of fraction
     sseg_unit_2 : entity work.hex_to_sseg
-        port map (hex => frac_out(7 downto 4), dp => '0', sseg => led2);
+        port map (hex => frac_out(7 downto 4), dp => '1', sseg => hex3);
 
-    -- sign
-    led3 <= "11111110" when sign_out = '1' else -- middle bar
-            "11111111"; -- blank
-
-    -- instantiate 7-seg LED display time-multiplexing module
-    disp_unit : entity work.disp_mux
-        port map (
-            clk => clk, reset => '0',
-            in0 => led0, in1 => led1, in2 => led2, in3 => led3,
-            an => an, sseg => sseg
-        );
+	 hex4 <= "01000000"; --zero com o ponto
+		  
+    -- sign 
+    hex5 <= "10111111" when sign_out = '1' else --negativo
+            "11111111"; -- apagado (positivo)
 end arch;
